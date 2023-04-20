b834c6cf2f5e0d23b022b69482e122f1ecafa859
Zeeeratul <celestdelahaye@gmail.com>
2023-04-20 20:35:29 +0200

    push

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import { getStockInformation } from "./stockInfo";
 import dotenv from "dotenv";
+import { PrismaClient } from "@prisma/client";
 
 dotenv.config();
+const prisma = new PrismaClient();
 
 async function start() {
   const data = await getStockInformation("AAPL");
   console.log(data);
+
+  await prisma.company.create({
+    data: {
+      income: data.incomeStatement,
+      balance: data.balanceSheet,
+    },
+  });
 }
 
 start();
--- src/stockInfo.ts
+++ src/stockInfo.ts
@@ -3,25 +3,20 @@ import axios from "axios";
 export async function getStockInformation(symbol: string) {
   const ALPHAVANTAGE_API_KEY = process.env.ALPHAVANTAGE_API_KEY;
 
-  console.log({ ALPHAVANTAGE_API_KEY });
+  const incomeStatementUrl = `https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol=${symbol}&apikey=${ALPHAVANTAGE_API_KEY}`;
+  const balanceSheetUrl = `https://www.alphavantage.co/query?function=BALANCE_SHEET&symbol=${symbol}&apikey=${ALPHAVANTAGE_API_KEY}`;
 
-  // const incomeStatementUrl = `https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol=${symbol}&apikey=${ALPHAVANTAGE_API_KEY}`;
-  // const balanceSheetUrl = `https://www.alphavantage.co/query?function=BALANCE_SHEET&symbol=${symbol}&apikey=${ALPHAVANTAGE_API_KEY}`;
-  // // const marketCapUrl = `https://api.polygon.io/v3/reference/tickers/AAPL?apiKey=${polygonApiKey}`;
+  try {
+    const response = await Promise.all([
+      await axios.get(incomeStatementUrl),
+      await axios.get(balanceSheetUrl),
+    ]);
 
-  // try {
-  //   const response = await Promise.all([
-  //     await axios.get(incomeStatementUrl),
-  //     await axios.get(balanceSheetUrl),
-  //     // await axios.get(marketCapUrl),
-  //   ]);
-
-  //   return {
-  //     ...response[0].data.annualReports[0],
-  //     ...response[1].data.annualReports[0],
-  //     // marketCap: response[2].data.results.market_cap,
-  //   };
-  // } catch (error) {
-  //   console.error("Error fetching stock information:", error.message);
-  // }
+    return {
+      incomeStatement: response[0].data.annualReports[0],
+      balanceSheet: response[1].data.annualReports[0],
+    };
+  } catch (error) {
+    console.error("Error fetching stock information:", error.message);
+  }
 }
